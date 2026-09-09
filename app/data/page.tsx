fd166bef00b0f47279b90052284a1701da99f7a3
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-09-09 12:58:05 +0530

    add a laoding state

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -6,8 +6,9 @@ import axios from "axios";
 export default async function User(){
 
     const res = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-    const data = res.data;
 
+    await new Promise(r=>setTimeout(r,5000));
+    const data = res.data;
 
     return <div>
         USER PAGE
