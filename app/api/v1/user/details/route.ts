b4b98201c3b12e3dd6c0c92862cd92d14868c935
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-09-09 18:40:09 +0530

    added backend code to out next.js server

---
+++ app/api/v1/user/details/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export function GET(){
+
+    return NextResponse.json({
+        user:"harkirat",
+        email:"harkirat@gmail.com"
+    })
+
+}
+
+export function POST(){
+
+
+    return NextResponse.json({
+        user:"harkirat",
+        email:"harkirat@gmail.com"
+    })
+
+
+}
+
+
+export function PUT(){
+
+
+    return NextResponse.json({
+        user:"harkirat",
+        email:"harkirat@gmail.com"
+    })
+
+
+}
--- app/data/page.tsx
+++ app/data/page.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export default async function User(){
 
-    const res = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+    const res = await axios.get("http://localhost:3000/api/v1/user/details")
 
     await new Promise(r=>setTimeout(r,5000));
     const data = res.data;
